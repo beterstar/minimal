b77098af75eabf1c111d6c9070f49048c455217f
bebaw013 <bebaw0132000@gmail.com>
2023-10-20 20:12:27 +0700

    second-commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const slider = document.getElementById('nav-slider');
 const berger = document.getElementById('berger');
 const exit = document.getElementById('exit');
+const button = document.getElementById('btn')
+const content_toggle = document.getElementById('content-toggle');
+let count = 0;
 
 berger.addEventListener('click',()=>{
     slider.classList.toggle('active');
@@ -11,4 +14,17 @@ exit.addEventListener('click',()=>{
 
 slider.addEventListener('mouseleave',()=>{
     slider.classList.remove("active");
+})
+
+
+button.addEventListener('click',()=>{
+    content_toggle.classList.toggle('content-active');
+    if((count % 2) == 0){
+        button.innerText = "Hide Detail";
+        count++
+    }
+    else if((count % 2) == 1){
+        button.innerText = "Show More Detail";
+        count++
+    }
 })
